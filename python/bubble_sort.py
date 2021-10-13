1451e0dd9e194372290aa6e30893ca3f0dfa7fa2
grunwaldcsaba <35535609+grunwaldcsaba@users.noreply.github.com>
2021-10-13 19:17:14 +0200

    Kommentek javítva

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,3 +1,5 @@
+
+
 nums = [1, 4, 2, 9, 3, 7, 5, 8, 6]
 
 def bubble_sort(nums):
@@ -5,7 +7,7 @@ def bubble_sort(nums):
     for i in range(n):
         for j in range(n-1):
             if (nums[j] > nums[j+1]):
-                swap(nums, j, j + 1)
+                nums[j], nums[j+1] = nums[j + 1], nums[j]
 
 
 def swap(nums, index1, index2):
@@ -13,6 +15,6 @@ def swap(nums, index1, index2):
     nums[index1] = nums[index2]
     nums[index2] = temp
 
-print("Before sorting: " + nums)
+print("Before sorting: ", nums)
 bubble_sort(nums)
-print("After sorting: " + nums)
+print("After sorting: ", nums)
